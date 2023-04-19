00efe6e111f3690c7a2de0a40147fd2c50445fda
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-04-19 16:01:20 -0700

    Update tempSumpFiBasicEps.sql

--- models/incremental/tempSumpFiBasicEps.sql
+++ models/incremental/tempSumpFiBasicEps.sql
@@ -7,6 +7,6 @@ SELECT
   sk_companyid,
   fi_qtr_start_date,
   sum(fi_basic_eps) OVER (PARTITION BY companyid ORDER BY fi_qtr_start_date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) - fi_basic_eps sum_fi_basic_eps
-FROM LIVE.Financial
-JOIN LIVE.DimCompany
+FROM {{ source('tpcdi', 'Financial') }}
+JOIN {{ ref('DimCompany') }}
   USING (sk_companyid);
