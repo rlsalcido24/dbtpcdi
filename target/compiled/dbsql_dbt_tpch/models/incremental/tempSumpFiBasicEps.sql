e32a6df68357e49e35bbf6a38bb8ade4ee68e234
shabbirk-db <91239704+shabbirk-db@users.noreply.github.com>
2023-04-21 09:59:17 +1000

    Delete target directory

--- target/compiled/dbsql_dbt_tpch/models/incremental/tempSumpFiBasicEps.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT
-  sk_companyid,
-  fi_qtr_start_date,
-  sum(fi_basic_eps) OVER (PARTITION BY companyid ORDER BY fi_qtr_start_date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) - fi_basic_eps sum_fi_basic_eps
-FROM `dbt_shabbirkdb`.`Financial`
-JOIN `dbt_shabbirkdb`.`DimCompany`
-  USING (sk_companyid);
--- target/compiled/dbsql_dbt_tpch/models/silver/AccountIncrementalStg.sql
+++
@@ -1,4 +0,0 @@
-SELECT 
-    md5(accountid::string) AS sk_customerid,
-    *
-FROM `roberto_salcido_tpcdi_dlt_advanced_10_wh`.`AccountIncremental`
