8f0e195765dbd192bb93c2706fca933d78b55765
Lorin Dawson <lorin@databricks.com>
2023-08-09 12:04:23 -0400

    completed Redshift/models/incremental/FactCashBalances.sql

--- Redshift/models/incremental/FactCashBalances.sql
+++ Redshift/models/incremental/FactCashBalances.sql
@@ -10,26 +10,26 @@ SELECT
     a.sk_customerid,
     a.sk_accountid,
     d.sk_dateid,
-    SUM(account_daily_total)
+    SUM(c.account_daily_total)
         OVER (
             PARTITION BY c.accountid
             ORDER BY
                 c.datevalue
             ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
         )
-    cash,
+    AS cash,
     c.batchid
 FROM (
     SELECT
-        ct_ca_id accountid,
+        ct_ca_id AS accountid,
         --to_date(ct_dts) datevalue,
-        TO_DATE(ct_dts, 'YYYYMMDD') datevalue,
-        SUM(ct_amt) account_daily_total,
+        TO_DATE(ct_dts, 'YYYYMMDD') AS datevalue,
+        SUM(ct_amt) AS account_daily_total,
         batchid
     FROM (
         SELECT
             *,
-            1 batchid
+            1 AS batchid
         FROM {{ source('tpcdi', 'CashTransactionHistory') }}
         --FROM prd.CashTransactionHistory
         UNION ALL
@@ -42,20 +42,22 @@ FROM (
             batchid
         FROM {{ ref('cashtransactionincremental') }}
     --FROM stg.CashTransactionIncremental
-    ) t
+    ) AS t
     GROUP BY
     --accountid,
         ct_ca_id,
         --datevalue,
         TO_DATE(ct_dts, 'YYYYMMDD'),
         batchid
-) c
-    JOIN {{ source('tpcdi', 'DimDate') }} d
-        --JOIN prd.DimDate d 
+) AS c
+    INNER JOIN {{ source('tpcdi', 'DimDate') }} AS d
+        --JOIN prd.DimDate d
         ON c.datevalue = d.datevalue
-    -- Converts to LEFT JOIN if this is run as DQ EDITION. On some higher Scale Factors, a small number of Account IDs are missing from dimaccount, causing audit check failures. 
-    LEFT JOIN {{ ref( 'dimaccount') }} a
-        -- LEFT JOIN dbo.dimaccount a 
+    -- Converts to LEFT JOIN if this is run as DQ EDITION. On some higher Scale
+    -- Factors, a small number of Account IDs are missing from dimaccount,
+    -- causing audit check failures.
+    LEFT JOIN {{ ref( 'dimaccount') }} AS a
+        -- LEFT JOIN dbo.dimaccount a
         ON
             c.accountid = a.accountid
             AND c.datevalue >= a.effectivedate
