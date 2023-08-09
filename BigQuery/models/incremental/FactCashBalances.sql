b3fe12ec52f795f0a6c2926721f65f9761eaca8d
Lorin Dawson <lorin@databricks.com>
2023-08-09 10:22:46 -0400

    completed BigQuery/models/incremental/FactCashBalances.sql

--- BigQuery/models/incremental/FactCashBalances.sql
+++ BigQuery/models/incremental/FactCashBalances.sql
@@ -8,7 +8,7 @@ SELECT
     a.sk_customerid,
     a.sk_accountid,
     d.sk_dateid,
-    SUM(account_daily_total)
+    SUM(c.account_daily_total)
         OVER (PARTITION BY c.accountid ORDER BY c.datevalue) AS cash,
     c.batchid
 FROM (
@@ -33,8 +33,9 @@ FROM (
 ) AS c
     INNER JOIN {{ source(var('benchmark'), 'DimDate') }} AS d
         ON c.datevalue = d.datevalue
-    -- Converts to LEFT JOIN if this is run as DQ EDITION. On some higher Scale Factors,
-    -- a small number of Account IDs are missing from DimAccount, causing audit check failures.
+    -- Converts to LEFT JOIN if this is run as DQ EDITION. On some higher Scale
+    -- Factors, a small number of Account IDs are missing from DimAccount,
+    -- causing audit check failures.
     LEFT JOIN {{ ref( 'DimAccount') }} AS a
         ON
             c.accountid = a.accountid
