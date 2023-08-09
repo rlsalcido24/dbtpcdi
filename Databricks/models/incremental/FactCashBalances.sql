8f0d8c191fb6606051661ba627080d9931c357c7
Lorin Dawson <lorin@databricks.com>
2023-08-09 09:33:35 -0400

    completed Databricks/models/incremental/FactCashBalances.sql

--- Databricks/models/incremental/FactCashBalances.sql
+++ Databricks/models/incremental/FactCashBalances.sql
@@ -8,20 +8,20 @@ SELECT
     a.sk_customerid,
     a.sk_accountid,
     d.sk_dateid,
-    SUM(account_daily_total)
+    SUM(c.account_daily_total)
         OVER (PARTITION BY c.accountid ORDER BY c.datevalue)
-    cash,
+    AS cash,
     c.batchid
 FROM (
     SELECT
-        ct_ca_id accountid,
-        TO_DATE(ct_dts) datevalue,
-        SUM(ct_amt) account_daily_total,
+        ct_ca_id AS accountid,
+        TO_DATE(ct_dts) AS datevalue,
+        SUM(ct_amt) AS account_daily_total,
         batchid
     FROM (
         SELECT
             *,
-            1 batchid
+            1 AS batchid
         FROM {{ source('tpcdi', 'CashTransactionHistory') }}
         UNION ALL
         SELECT * EXCEPT (cdc_flag, cdc_dsn)
@@ -31,11 +31,13 @@ FROM (
         accountid,
         datevalue,
         batchid
-) c
-    JOIN {{ source('tpcdi', 'DimDate') }} d
+) AS c
+    INNER JOIN {{ source('tpcdi', 'DimDate') }} AS d
         ON c.datevalue = d.datevalue
-    -- Converts to LEFT JOIN if this is run as DQ EDITION. On some higher Scale Factors, a small number of Account IDs are missing from DimAccount, causing audit check failures. 
-    LEFT JOIN {{ ref( 'DimAccount') }} a
+    -- Converts to LEFT JOIN if this is run as DQ EDITION. On some higher Scale
+    -- Factors, a small number of Account IDs are missing from DimAccount,
+    -- causing audit check failures.
+    LEFT JOIN {{ ref( 'DimAccount') }} AS a
         ON
             c.accountid = a.accountid
             AND c.datevalue >= a.effectivedate
