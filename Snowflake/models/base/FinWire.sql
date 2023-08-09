69d283d5f81b90c11da1631375a4a53626fc831f
Lorin Dawson <lorin@databricks.com>
2023-08-09 18:19:34 -0400

    completed Snowflake/models/base/FinWire.sql

--- Snowflake/models/base/FinWire.sql
+++ Snowflake/models/base/FinWire.sql
@@ -6,8 +6,8 @@
 }}
 
 SELECT
-    $1 value,
-    SUBSTR($1, 16, 3) rectype
+    $1 AS value, -- noqa: RF04
+    SUBSTR($1, 16, 3) AS rectype
 FROM
     @{{ var('stage') }}/Batch1 (
         FILE_FORMAT => 'TXT_FIXED_WIDTH', PATTERN => '.*FINWIRE.*'
