f3eab5aeb4cc87a44aafb7c82d57998d1a6ba74f
Lorin Dawson <lorin@databricks.com>
2023-08-09 11:08:09 -0400

    BigQuery/models/incremental/tempSumpFiBasicEps.sql

--- BigQuery/models/incremental/tempSumpFiBasicEps.sql
+++ BigQuery/models/incremental/tempSumpFiBasicEps.sql
@@ -4,14 +4,15 @@
     )
 }}
 SELECT
-    sk_companyid,
-    fi_qtr_start_date,
-    SUM(fi_basic_eps)
+    f.sk_companyid,
+    f.fi_qtr_start_date,
+    SUM(fi.fi_basic_eps)
         OVER (
-            PARTITION BY companyid
-            ORDER BY fi_qtr_start_date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+            PARTITION BY dc.companyid
+            ORDER BY f.fi_qtr_start_date
+            ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
         )
-    - fi_basic_eps AS sum_fi_basic_eps
-FROM {{ ref('Financial') }}
-    INNER JOIN {{ ref('DimCompany') }}
-        ON sk_companyid = sk_companyid
+    - f.fi_basic_eps AS sum_fi_basic_eps
+FROM {{ ref('Financial') }} AS f
+    INNER JOIN {{ ref('DimCompany') }} AS dc
+        ON f.sk_companyid = dc.sk_companyid
