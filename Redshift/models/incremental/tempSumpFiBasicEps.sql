e8a30fc6757b622efbe09d6f1e39d18880816bd3
Lorin Dawson <lorin@databricks.com>
2023-08-09 13:07:37 -0400

    Redshift/models/incremental/tempSumpFiBasicEps.sql

--- Redshift/models/incremental/tempSumpFiBasicEps.sql
+++ Redshift/models/incremental/tempSumpFiBasicEps.sql
@@ -9,13 +9,14 @@
 SELECT
     f.sk_companyid,
     f.fi_qtr_start_date,
-    SUM(fi_basic_eps)
+    SUM(f.fi_basic_eps)
         OVER (
-            PARTITION BY companyid
-            ORDER BY fi_qtr_start_date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+            PARTITION BY c.companyid
+            ORDER BY f.fi_qtr_start_date
+            ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
         )
-    - fi_basic_eps sum_fi_basic_eps
-FROM {{ ref('financial') }} f
-    JOIN {{ ref('dimcompany') }} c
+    - f.fi_basic_eps AS sum_fi_basic_eps
+FROM {{ ref('financial') }} AS f
+    INNER JOIN {{ ref('dimcompany') }} AS c
         ON f.sk_companyid = c.sk_companyid
 --  USING (sk_companyid);
