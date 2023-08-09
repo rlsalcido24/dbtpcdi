4af84973809d4670b73c0b9c07669e95fe8a6256
Lorin Dawson <lorin@databricks.com>
2023-08-09 10:24:32 -0400

    wip BigQuery/models/incremental/FactHoldings.sql

--- BigQuery/models/incremental/FactHoldings.sql
+++ BigQuery/models/incremental/FactHoldings.sql
@@ -5,16 +5,16 @@
 }}
 
 SELECT
-    hh_h_t_id AS tradeid,
-    hh_t_id AS currenttradeid,
+    hh.hh_h_t_id AS tradeid,
+    hh.hh_t_id AS currenttradeid,
     sk_customerid,
     sk_accountid,
     sk_securityid,
     sk_companyid,
     sk_closedateid AS sk_dateid,
     sk_closetimeid AS sk_timeid,
-    tradeprice AS currentprice,
-    hh_after_qty AS currentholding,
+    dt.tradeprice AS currentprice,
+    hh.hh_after_qty AS currentholding,
     hh.batchid
 FROM (
     SELECT
@@ -25,8 +25,8 @@ FROM (
     SELECT * EXCEPT (cdc_flag, cdc_dsn)
     FROM {{ ref('HoldingIncremental') }}
 ) AS hh
--- Converts to LEFT JOIN if this is run as DQ EDITION.
--- It is possible, because of the issues upstream with DimSecurity/DimAccount on "some" scale factors,
+-- Converts to LEFT JOIN if this is run as DQ EDITION. It is possible, because
+-- of the issues upstream with DimSecurity/DimAccount on "some" scale factors,
 -- that DimTrade may be missing some rows.
 --${dq_left_flg}
     LEFT JOIN {{ ref('DimTrade') }} AS dt
