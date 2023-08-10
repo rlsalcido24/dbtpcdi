88ced33a3b34a196a4c19bb02fab1cd380e7b6d1
Lorin Dawson <lorin@databricks.com>
2023-08-10 10:54:16 -0400

    wip Snowflake/models/incremental/FactHoldings.sql

--- Snowflake/models/incremental/FactHoldings.sql
+++ Snowflake/models/incremental/FactHoldings.sql
@@ -3,29 +3,30 @@
         materialized = 'table'
     )
 }}
-
 SELECT
-    hh_h_t_id tradeid,
-    hh_t_id currenttradeid,
+    hh.hh_h_t_id AS tradeid,
+    hh.hh_t_id AS currenttradeid,
     sk_customerid,
     sk_accountid,
     sk_securityid,
     sk_companyid,
-    sk_closedateid sk_dateid,
-    sk_closetimeid sk_timeid,
-    tradeprice currentprice,
-    hh_after_qty currentholding,
+    sk_closedateid AS sk_dateid,
+    sk_closetimeid AS sk_timeid,
+    tradeprice AS currentprice,
+    hh.hh_after_qty AS currentholding,
     hh.batchid
 FROM (
     SELECT
         *,
-        1 batchid
+        1 AS batchid
     FROM {{ source('tpcdi', 'HoldingHistory') }}
     UNION ALL
     SELECT * EXCLUDE (cdc_flag, cdc_dsn)
     FROM {{ ref('HoldingIncremental') }}
-) hh
--- Converts to LEFT JOIN if this is run as DQ EDITION. It is possible, because of the issues upstream with DimSecurity/DimAccount on "some" scale factors, that DimTrade may be missing some rows.
+) AS hh
+-- Converts to LEFT JOIN if this is run as DQ EDITION. It is possible, because
+-- of the issues upstream with DimSecurity/DimAccount on "some" scale factors,
+-- that DimTrade may be missing some rows.
 --${dq_left_flg}
-    LEFT JOIN {{ ref('DimTrade') }} dt
-        ON tradeid = hh_t_id
+    LEFT JOIN {{ ref('DimTrade') }} AS dt
+        ON dt.tradeid = hh.hh_t_id
