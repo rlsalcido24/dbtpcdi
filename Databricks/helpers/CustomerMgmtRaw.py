fac80adf5357afb1eb70ff68a310d19dd8e643ea
Lorin Dawson <lorin@databricks.com>
2023-08-24 13:16:19 -0400

    Databricks flake8 compliant

--- Databricks/helpers/CustomerMgmtRaw.py
+++ Databricks/helpers/CustomerMgmtRaw.py
@@ -48,14 +48,14 @@ spark.sql(f"CREATE DATABASE IF NOT EXISTS {staging_db}")
 spark.sql(
     f"""
   CREATE TABLE IF NOT EXISTS {staging_db}.CustomerMgmt PARTITIONED BY (ActionType) TBLPROPERTIES (
-    --delta.tuneFileSizesForRewrites = true, 
+    --delta.tuneFileSizesForRewrites = true,
     delta.autoOptimize.optimizeWrite = false
-  ) AS SELECT 
-    cast(Customer._C_ID as BIGINT) customerid, 
+  ) AS SELECT
+    cast(Customer._C_ID as BIGINT) customerid,
     cast(Customer.Account._CA_ID as BIGINT) accountid,
-    cast(Customer.Account.CA_B_ID as BIGINT) brokerid, 
+    cast(Customer.Account.CA_B_ID as BIGINT) brokerid,
     nullif(Customer._C_TAX_ID, '') taxid,
-    nullif(Customer.Account.CA_NAME, '') accountdesc, 
+    nullif(Customer.Account.CA_NAME, '') accountdesc,
     cast(Customer.Account._CA_TAX_ST as TINYINT) taxstatus,
     decode(_ActionType,
       "NEW","Active",
@@ -64,18 +64,18 @@ spark.sql(
       "UPDCUST","Active",
       "CLOSEACCT","Inactive",
       "INACT","Inactive") status,
-    nullif(Customer.Name.C_L_NAME, '') lastname, 
-    nullif(Customer.Name.C_F_NAME, '') firstname, 
-    nullif(Customer.Name.C_M_NAME, '') middleinitial, 
+    nullif(Customer.Name.C_L_NAME, '') lastname,
+    nullif(Customer.Name.C_F_NAME, '') firstname,
+    nullif(Customer.Name.C_M_NAME, '') middleinitial,
     nullif(upper(Customer._C_GNDR), '') gender,
-    cast(Customer._C_TIER as TINYINT) tier, 
+    cast(Customer._C_TIER as TINYINT) tier,
     cast(Customer._C_DOB as DATE) dob,
-    nullif(Customer.Address.C_ADLINE1, '') addressline1, 
-    nullif(Customer.Address.C_ADLINE2, '') addressline2, 
+    nullif(Customer.Address.C_ADLINE1, '') addressline1,
+    nullif(Customer.Address.C_ADLINE2, '') addressline2,
     nullif(Customer.Address.C_ZIPCODE, '') postalcode,
-    nullif(Customer.Address.C_CITY, '') city, 
+    nullif(Customer.Address.C_CITY, '') city,
     nullif(Customer.Address.C_STATE_PROV, '') stateprov,
-    nullif(Customer.Address.C_CTRY, '') country, 
+    nullif(Customer.Address.C_CTRY, '') country,
     nvl2(
       nullif(Customer.ContactInfo.C_PHONE_1.C_LOCAL, ''),
       concat(
@@ -102,8 +102,8 @@ spark.sql(
       cast(null as string)) phone3,
     nullif(Customer.ContactInfo.C_PRIM_EMAIL, '') email1,
     nullif(Customer.ContactInfo.C_ALT_EMAIL, '') email2,
-    nullif(Customer.TaxInfo.C_LCL_TX_ID, '') lcl_tx_id, 
-    nullif(Customer.TaxInfo.C_NAT_TX_ID, '') nat_tx_id, 
+    nullif(Customer.TaxInfo.C_LCL_TX_ID, '') lcl_tx_id,
+    nullif(Customer.TaxInfo.C_NAT_TX_ID, '') nat_tx_id,
     to_timestamp(_ActionTS) update_ts,
     _ActionType ActionType
   FROM v_CustomerMgmt
