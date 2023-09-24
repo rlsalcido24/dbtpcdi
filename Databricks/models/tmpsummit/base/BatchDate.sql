06a617ce3b6907008e0316eb6899d72e2de8f3fc
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-09-23 17:34:07 -0700

    Update BatchDate.sql

--- Databricks/models/tmpsummit/base/BatchDate.sql
+++ Databricks/models/tmpsummit/base/BatchDate.sql
@@ -1,9 +1,26 @@
 {{
     config(
-        materialized = 'view'
+        materialized = 'streaming_table'
     )
 }}
 
-select
-    * from {{ var('catalog') }}.{{ var('prodschema') }}.batchdate
+SELECT
+  *,
+  cast(
+    substring(
+      _metadata.file_path
+      FROM
+        (position('/Batch', _metadata.file_path) + 6) FOR 1
+    ) as int
+  ) batchid
+FROM
+  STREAM read_files(
+    "/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf={{ var('scalefactor') }}/Batch*",
+    format => "csv",
+    inferSchema => False,
+    header => 'False',
+    sep => '|',
+    fileNamePattern => "BatchDate.txt",
+    schema => "batchdate DATE NOT NULL COMMENT 'Batch date'"
+  )
 
