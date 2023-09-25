0787a8b1b514e310770f918915b243c36f6b7aab
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-09-25 08:26:59 -0700

    Update FinWire.sql

--- Databricks/models/tmpsummit/base/FinWire.sql
+++ Databricks/models/tmpsummit/base/FinWire.sql
@@ -5,4 +5,4 @@
     )
 }}
 
-SELECT value, substring(value, 16, 3) rectype FROM (   SELECT value FROM text.`/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf="{{var('scalefactor')}}"/Batch1/FINWIRE[0-9][0-9][0-9][0-9]Q[1-4]`)
+SELECT value, substring(value, 16, 3) rectype FROM (   SELECT value FROM text.`/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf={{var('scalefactor')}}/Batch1/FINWIRE[0-9][0-9][0-9][0-9]Q[1-4]`)
