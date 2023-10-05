8816fac6bd90ed09b91265aa8ca3fffc3fda0676
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-10-05 08:58:26 -0700

    Update TradeType.sql

--- Databricks/models/main/incremental/TradeType.sql
+++ Databricks/models/main/incremental/TradeType.sql
@@ -1,9 +1,18 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'streaming_table'
     )
 }}
-select
-    *
-from
-    {{ source('tpcdi', 'TradeType') }}
+SELECT
+  *
+FROM
+  STREAM read_files(
+    "/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf={{ var('scalefactor') }}/Batch1",
+    format => "csv",
+    inferSchema => False,
+    header => 'False',
+    sep => '|',
+    fileNamePattern => "TradeType.txt",
+    schema => "tt_id STRING NOT NULL COMMENT 'Trade type code', tt_name STRING NOT NULL COMMENT 'Trade type description', tt_is_sell INT NOT NULL COMMENT 'Flag indicating a sale', tt_is_mrkt INT NOT NULL COMMENT 'Flag indicating a market order'"
+  )
+
