3ba034d0b9a99fecef8a7a0fa23160c74bea4f10
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-10-05 08:58:09 -0700

    Update TaxRate.sql

--- Databricks/models/main/incremental/TaxRate.sql
+++ Databricks/models/main/incremental/TaxRate.sql
@@ -1,9 +1,17 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'streaming_table'
     )
 }}
-select
-    *
-from
-    {{ source('tpcdi', 'TaxRate') }}
+SELECT
+  *
+FROM
+  STREAM read_files(
+    "/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf={{ var('scalefactor') }}/Batch1",
+    format => "csv",
+    inferSchema => False,
+    header => 'False',
+    sep => '|',
+    fileNamePattern => "TaxRate.txt",
+    schema => "tx_id STRING NOT NULL COMMENT 'Tax rate code', tx_name STRING NOT NULL COMMENT 'Tax rate description', tx_rate FLOAT NOT NULL COMMENT 'Tax rate'"
+  )
