72f1f9fb2dee18d88bd2a5b5e3ceca3700384c71
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-10-11 15:16:33 -0700

    Update Industry.sql

--- Databricks/models/main/incremental/Industry.sql
+++ Databricks/models/main/incremental/Industry.sql
@@ -1,18 +1,10 @@
 {{
     config(
-        materialized = 'streaming_table'
+        materialized = 'table'
     )
 }}
-SELECT
-  *
-FROM
-  STREAM read_files(
-    "/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf={{ var('scalefactor') }}/Batch1",
-    format => "csv",
-    inferSchema => False,
-    header => 'False',
-    sep => '|',
-    fileNamePattern => "Industry.txt",
-    schema => "in_id STRING NOT NULL COMMENT 'Industry code', in_name STRING NOT NULL COMMENT 'Industry description', in_sc_id STRING NOT NULL COMMENT 'Sector identifier'"
-  )
+select
+    *
+from
+    {{ source('tpcdi', 'Industry') }}
 
