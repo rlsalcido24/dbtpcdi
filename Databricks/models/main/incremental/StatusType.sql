9c91cc59ccacd458eec6ba44653137615bf48c6c
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-10-11 15:16:54 -0700

    Update StatusType.sql

--- Databricks/models/main/incremental/StatusType.sql
+++ Databricks/models/main/incremental/StatusType.sql
@@ -1,17 +1,9 @@
 {{
     config(
-        materialized = 'streaming_table'
+        materialized = 'table'
     )
 }}
-SELECT
-  *
-FROM
-  STREAM read_files(
-    "/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf={{ var('scalefactor') }}/Batch1",
-    format => "csv",
-    inferSchema => False,
-    header => 'False',
-    sep => '|',
-    fileNamePattern => "StatusType.txt",
-    schema => "st_id STRING NOT NULL COMMENT 'Status code', st_name STRING NOT NULL COMMENT 'Status description'"
-  )
+select
+    *
+from
+    {{ source('tpcdi', 'StatusType') }}
