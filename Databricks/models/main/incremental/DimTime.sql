1c467ad4f92010fb9b2e22b14040f8bd3f105e98
rlsalcido24 <rsalcido@alumni.stanford.edu>
2023-10-11 15:16:14 -0700

    Update DimTime.sql

--- Databricks/models/main/incremental/DimTime.sql
+++ Databricks/models/main/incremental/DimTime.sql
@@ -1,17 +1,9 @@
 {{
     config(
-        materialized = 'streaming_table'
+        materialized = 'table'
     )
 }}
-SELECT
-  *
-FROM
-  STREAM read_files(
-    "/Volumes/tpcdi/tpcdi_raw_data/tpcdi_volume/sf={{ var('scalefactor') }}/Batch1",
-    format => "csv",
-    inferSchema => False,
-    header => 'False',
-    sep => '|',
-    fileNamePattern => "Time.txt",
-    schema => "sk_timeid BIGINT NOT NULL COMMENT 'Surrogate key for the time', timevalue STRING NOT NULL COMMENT 'The time stored appropriately for doing', hourid INT NOT NULL COMMENT 'Hour number as a number, e.g. 01', hourdesc STRING NOT NULL COMMENT 'Hour number as text, e.g. 01', minuteid INT NOT NULL COMMENT 'Minute as a number, e.g. 23', minutedesc STRING NOT NULL COMMENT 'Minute as text, e.g. 01:23', secondid INT NOT NULL COMMENT 'Second as a number, e.g. 45', seconddesc STRING NOT NULL COMMENT 'Second as text, e.g. 01:23:45', markethoursflag BOOLEAN COMMENT 'Indicates a time during market hours', officehoursflag BOOLEAN COMMENT 'Indicates a time during office hours'"
-  )
+select
+    *
+from
+    {{ source('tpcdi', 'DimTime') }}
